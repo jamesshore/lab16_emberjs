c78b756069001f766d4324ac6a74c5aa861a0276
James Shore <jshore@jamesshore.com>
2015-02-24 12:08:20 -0800

    Initial commit (based on automatopia project)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node generated/dist/run.js $PORT
---
+++ src/client/example.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+(function() {
+	"use strict";
+
+}());
