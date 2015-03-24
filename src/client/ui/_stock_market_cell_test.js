d0a546e690e8b2d65b298928f3b05a485c07da39
James Shore <jshore@jamesshore.com>
2015-03-23 21:30:55 -0700

    Attempting to test stock-market-cell component; getting weird error

---
+++ src/client/ui/_stock_market_cell_test.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	describeComponent("stock-market-cell", "StockMarketCell", {}, function() {
+
+		var component;
+		var $;
+
+		beforeEach(function() {
+			component = this.subject();
+			$ = this.$();
+		});
+
+		it("renders", function() {
+			expect(component.tagName).to.equal("td");
+			//expect($.html()).to.equal(
+			//	"<td>2010</td>\n" +
+			//	"<td>$10,000</td>\n" +
+			//	"<td>$7,000</td>\n" +
+			//	'<td class="negative">($695)</td>\n' +
+			//	'<td class="negative">($232)</td>\n' +
+			//	"<td>$907</td>\n" +
+			//	"<td>$9,981</td>\n"
+			//);
+		});
+
+	});
+
+}());
