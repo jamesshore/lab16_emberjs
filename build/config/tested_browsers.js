e1974d2671c832cef211371899f74a2e1487f883
James Shore <jshore@jamesshore.com>
2015-03-24 10:57:35 -0700

    Build fails unless all browsers are tested

--- build/config/tested_browsers.js
+++ build/config/tested_browsers.js
@@ -5,12 +5,12 @@
 	// Uncomment and modify the following list to cause the build to fail unless these browsers are tested.
 
 	module.exports = [
-		//"IE 8.0.0 (Windows XP)",
-		//"IE 9.0.0 (Windows 7)",
-		//"Firefox 23.0.0 (Mac OS X 10.8)",
-		//"Chrome 29.0.1547 (Mac OS X 10.8.5)",
-		//"Safari 6.0.5 (Mac OS X 10.8.5)",
-		//"Mobile Safari 6.0.0 (iOS 6.1)"
+		"IE 9.0.0 (Windows 7)",
+		"IE 11.0.0 (Windows 7)",
+		"Firefox 36.0.0 (Mac OS X 10.10)",
+		"Chrome 41.0.2272 (Mac OS X 10.10.2)",
+		"Safari 8.0.4 (Mac OS X 10.10.2)",
+		"Mobile Safari 8.0.0 (iOS 10.10.2)"
 	];
 
 }());
