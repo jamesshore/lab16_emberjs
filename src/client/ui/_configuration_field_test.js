7681e8f1f7ddcf4b4358bef25fd2470ccdbc8fb1
James Shore <jshore@jamesshore.com>
2015-03-25 12:05:01 -0700

    Stubbed-in ConfigurationField test

---
+++ src/client/ui/_configuration_field_test.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	var UserEnteredDollars = require("../values/user_entered_dollars.js");
+
+	describeComponent("configuration-field", "ConfigurationField", {}, function() {
+
+		var component;
+		var $;
+
+		beforeEach(function() {
+			component = this.subject();
+			Ember.run(function() {
+				component.set("value", new UserEnteredDollars(""));
+			});
+			$ = this.$();
+		});
+
+	});
+
+}());
---
+++ src/client/ui/configuration_field.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2015 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
+(function() {
+	"use strict";
+
+	module.exports = Ember.Component.extend({
+	});
+
+}());
