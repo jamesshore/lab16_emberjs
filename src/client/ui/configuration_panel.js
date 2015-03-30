d4356943c2b7a2b47630f313cf697fa8e8a0b03f
James Shore <jshore@jamesshore.com>
2015-03-30 15:44:24 -0700

    ConfigurationPanel updates when user configuration updates

--- src/client/ui/configuration_panel.js
+++ src/client/ui/configuration_panel.js
@@ -5,29 +5,29 @@
 	module.exports = Ember.Component.extend({
 		_configWatcher: function() {
 			var self = this;
-			config(this).onChange("configuration_panel", function() {
-				self.notifyPropertyChange("value");
+			var userConfig = config(this);
+			if (userConfig) userConfig.onChange("configuration_panel", function() {
+				self.notifyPropertyChange("_value");
 			});
-		}.observes("value"),
+		}.observes("value").on("init"),
 
 		startingBalance: function(key, newValue, previousValue) {
 			if (arguments.length > 1) config(this).setStartingBalance(newValue);
 
 			return config(this).getStartingBalance();
-		}.property("value"),
+		}.property("_value"),
 
 		startingCostBasis: function(key, newValue, previousValue) {
 			if (arguments.length > 1) config(this).setStartingCostBasis(newValue);
 
 			return config(this).getStartingCostBasis();
-		}.property("value"),
+		}.property("_value"),
 
 		yearlySpending: function(key, newValue, previousValue) {
 			if (arguments.length > 1) config(this).setYearlySpending(newValue);
 
 			return config(this).getYearlySpending();
-		}.property("value")
-
+		}.property("_value")
 
 	});
 
